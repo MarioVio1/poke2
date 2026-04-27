0e38c58d3fe60a66cbeb602d912217c29d1a6762
Federico <federico@poke.com>
2026-04-27 20:05:01 +0200

    Added PNG sprites for first 11 besti

--- upload.bat
+++ upload.bat
@@ -3,7 +3,7 @@ cd G:\baxk\poke-main
 git config user.email "federico@poke.com"
 git config user.name "Federico"
 git add -A
-git commit -m "Added PNG sprites for first 11 besti"
-git push origin main
+git commit -m "Added PNG sprites for first 11 besti" 2>nul
+git push origin master
 echo FATTO!
 pause
